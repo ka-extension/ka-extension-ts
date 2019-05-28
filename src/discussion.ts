318e408f844eace7a1e831236d55ce4bc4685156
Matthias <MatthiasSaihttam1@gmail.com>
2019-05-28 13:41:28 -0700

    Add some discussion buttons listeners
    Find all discussion tab buttons (Questions, T&T, Help Requests), and add event listeners to them to trigger looking for new comments.
    Find the Load More Comments button and attach a listener to that.

--- src/discussion.ts
+++ src/discussion.ts
@@ -8,8 +8,14 @@ import {
 function updateComments (): void {
 	querySelectorAllPromise(`div[data-test-id='discussion-post']:not(.${EXTENSION_COMMENT_CLASSNAME})`, 100)
 		.then((unalteredComments: NodeList) => {
+			//Find the load more comments button, and attach an event listener to it.
+			//TODO: Make not trash
+			//TODO: Fails if there are no more comments; i.e., no button
+			Array.from((unalteredComments[0].parentNode!.parentNode!.parentNode!.parentNode as HTMLDivElement).children).slice(-1)[0].addEventListener("click", updateComments);
+			
 			for (let i = 0; i < unalteredComments.length; i++) {
 				const comment = unalteredComments[i] as HTMLElement;
+				//TODO: Fails if there's a reply on the page (is a 'discussion-post'; not extension modified; no link; no way the id; generally annoying.)
 				const flagControls = comment.querySelector("button[id^=uid-discussion-list-][id$=-flag]");
 				//Extract the comment id from the link to the comment
 				const commentId = (comment.querySelector("a[href*=qa_expand_key]") as HTMLAnchorElement).href.match(/qa_expand_key=(.*?)(?:&|$)/)![1];
@@ -28,15 +34,28 @@ function updateComments (): void {
 }
 
 function commentsButtonEventListener (uok: UsernameOrKaid): void {
-	querySelectorPromise(".button_1eqj1ga-o_O-shared_1t8r4tr-o_O-default_9fm203", 100)
-		.then(button => {
-			button.addEventListener("click", updateComments);
-			updateComments();
-		}).catch(console.error);
+	//Select tab buttons. The check for being on a discussion page saw that these were loaded
+	let discussionTabs = document.querySelectorAll("[data-test-id=\"discussion-tab\"]");
+	
+	if (!discussionTabs || !discussionTabs.length) {
+		console.error("Discussion tabs were loaded but now aren't.");
+	}
+	
+	//Listeners for switching discussion tabs
+	Array.from(discussionTabs).map(discussionTab => discussionTab as HTMLButtonElement).forEach(discussionTab =>
+		discussionTab.addEventListener("click", () => {
+			if (!discussionTab.classList.contains("kae-discussion-tab-selected")) {
+				Array.from(document.querySelectorAll(".kae-discussion-tab-selected")).forEach(el => el.classList.remove("kae-discussion-tab-selected"));
+				discussionTab.classList.add("kae-discussion-tab-selected");
+				updateComments();
+			}
+		})
+	)
 }
 
 function switchToTipsAndThanks () {
 	querySelectorPromise("#ka-uid-discussiontabbedpanel-1--tabbedpanel-tab-1").then(tabButton => tabButton as HTMLButtonElement).then(tabButton => {
+		//TODO: Don't switch to T&T if you have followed a direct link to question
 		const y = document.body.scrollTop;
 
 		tabButton.dispatchEvent(new MouseEvent("click", { "view": window, "bubbles": true }));
