011a99d76b34ddc32732f1ee7cb4b509f0d42886
Matthias <MatthiasSaihttam1@gmail.com>
2019-05-30 08:44:49 -0700

    Add check for replies button & refactor
    -Add an event listener to the show replies button
    -Move code that handles top-level comments into a specific check
    -Add the number of flags to a comment even if there are no flags (makes it clear when the Exension's checking flags)
    -Add a bunch of comments

--- src/discussion.ts
+++ src/discussion.ts
@@ -6,31 +6,57 @@ import {
 } from "./types/names";
 
 function updateComments (): void {
-	querySelectorAllPromise(`div[data-test-id='discussion-post']:not(.${EXTENSION_COMMENT_CLASSNAME})`, 100)
+	console.log("Checking for new comments");
+	
+	//TODO: Add a listener to the post button so new comments are found
+	//TODO: Bug: Opening a direct link to comment causes the replies to be automatically unfolded, and they aren't found
+	querySelectorAllPromise(`div[data-test-id='discussion-post']:not(.${EXTENSION_COMMENT_CLASSNAME})`, 50 /*ms*/, 20 /*attempts*/)
 		.then((unalteredComments: NodeList) => {
+			console.log("Found new comments");
 			//Find the load more comments button, and attach an event listener to it.
-			//TODO: Make not trash
-			//TODO: Fails if there are no more comments; i.e., no button
-			Array.from((unalteredComments[0].parentNode!.parentNode!.parentNode!.parentNode as HTMLDivElement).children).slice(-1)[0].addEventListener("click", updateComments);
+			//TODO: refactor
+			//TODO: fails if there are no more comments; i.e., no button
+			//TODO: fails if replies have just been opened, in which case the parentNode chain selects the wrong thelment.
+			// Array.from((unalteredComments[0].parentNode!.parentNode!.parentNode!.parentNode as HTMLDivElement).children).slice(-1)[0].addEventListener("click", () => { console.log("1"); updateComments(); });
 			
+			//Loop through all unalteredComments and apply any modifications to them
 			for (let i = 0; i < unalteredComments.length; i++) {
 				const comment = unalteredComments[i] as HTMLElement;
-				//TODO: Fails if there's a reply on the page (is a 'discussion-post'; not extension modified; no link; no way the id; generally annoying.)
-				const flagControls = comment.querySelector("button[id^=uid-discussion-list-][id$=-flag]");
-				//Extract the comment id from the link to the comment
-				const commentId = (comment.querySelector("a[href*=qa_expand_key]") as HTMLAnchorElement).href.match(/qa_expand_key=(.*?)(?:&|$)/)![1];
-
-				getComment(commentId).then((data) => {
-					if (flagControls && data && data.flags && data.flags.length) {
-						const flagsText = flagControls.querySelector("div[class^=default_olfzxm-o_O-flagText_1ajw024]");
-						flagsText!.textContent += ` (${data.flags.length})`;
-						flagControls.setAttribute("title", data.flags.join("\n"));
+				const flagButton = comment.querySelector("button[id^=uid-discussion-list-][id$=-flag]");
+				const timestampLink = comment.querySelector("a[href*=qa_expand_key]");
+				
+				//Replies to comments don't have a hyperlink or ID
+				if (timestampLink) {
+					//Extract the comment id from the link to the comment
+					const commentId = (timestampLink as HTMLAnchorElement)!.href.match(/qa_expand_key=(.*?)(?:&|$)/)![1];
+					
+					getComment(commentId).then((data) => {
+						if (flagButton && data && data.flags) {
+							const flagText = flagButton.querySelector("div");
+							flagText!.textContent += ` (${data.flags.length})`;
+							flagButton.setAttribute("title", data.flags.join("\n"));
+						}
+					}).catch(console.error);
+					
+					//Look for replies button and add an event listener to it
+					//TODO: Button for answers to questions ("Replies")
+					const showCommentsButton = comment.querySelector("[aria-controls*=-replies-container]");
+					if (showCommentsButton) {
+						//{ once: true } so that the listener is automatically removed after firing once
+						//Replies, once loaded once, are kept in the DOM tree, just hidden
+						showCommentsButton.addEventListener("click", updateComments, { once: true });
+					}else {
+						console.error("Replies button expected under top-level comment.");
 					}
-				}).catch(console.error);
+				}
 
 				comment.classList.add(EXTENSION_COMMENT_CLASSNAME);
 			}
-		}).catch(console.error);
+		}).catch(() => {
+			//There are known cases where updateComments is called at the possiblity of new comments
+			//Not finding new comments is fine
+			console.log("Didn't find new comments.");
+		});
 }
 
 function commentsButtonEventListener (uok: UsernameOrKaid): void {
