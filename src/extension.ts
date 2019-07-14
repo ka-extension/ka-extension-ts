a79c8a3cc0b5570393128564332e5935cacf11d8
Matthias <MatthiasSaihttam1@gmail.com>
2019-07-13 17:44:07 -0700

    Remove unused KA define code
    KAdefine is finally completely removed from the codebase. F.
    Also removes HoverQtipOptions which should have been removed with the hover card code

--- src/extension.ts
+++ src/extension.ts
@@ -2,33 +2,6 @@ import { UsernameOrKaid, Program } from "./types/data";
 import { querySelectorPromise } from "./util/promise-util";
 import { ScratchpadUI } from "./types/data";
 
-interface HoverQtipOptions {
-	my: string;
-	at: string;
-}
-
-interface KAdefineResult {
-	data?: KAdefineData;
-	ScratchpadUI?: ScratchpadUI;
-	createHoverCardQtip?: (target: HTMLElement, options: HoverQtipOptions) => void;
-	getKaid? (): string;
-}
-
-interface KAdefineData {
-	which?: string;
-	focusId?: string;
-	focusKind?: string;
-	isScratchpad?: boolean;
-}
-
-const KAdefine = {
-	asyncRequire (url: string, interval: number = 100, test?: (data: KAdefineResult) => boolean, maxAttempts?: number): Promise<KAdefineResult> {
-		return new Promise ((resolve, reject) => {
-			reject(new Error("KAdefine.asyncRequire is depreciated after KA removed `KAdefine`."));
-		});
-	}
-};
-
 const getScratchpadUI = (): Promise<ScratchpadUI> =>
 	new Promise((resolve, reject) => {
 		//Give it 10 seconds, max
@@ -123,4 +96,4 @@ abstract class Extension {
 	}
 }
 
-export { Extension, KAdefine };
+export { Extension };
