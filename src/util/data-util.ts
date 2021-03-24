8cab9b362efbfad6652cd534b9899fb08277ec3a
An0n3m0us <an0n3m0us@hotmail.com>
2021-03-24 21:07:02 +0000

    Remove blank lines

--- src/util/data-util.ts
+++ src/util/data-util.ts
@@ -2,13 +2,11 @@ function getKAID (): string {
 	// Get KAID from session
 	const kaids = localStorage["ka:4:mastery_accelerant_prompt"].match(/kaid_\d+/g);
 	const dates = localStorage["ka:4:mastery_accelerant_prompt"].match(/:\d{13}\}/g);
-
 	const combined = [];
 	for (let i = 0; i < kaids.length; i += 1) {
 			combined.push(dates[i] + kaids[i]);
 	}
 	combined.sort();
-
 	return combined[combined.length-1].match(/kaid_\d+/g)[0];
 }
 
