7ca5f39c7bb9fae64723eda5ec0c72dbd1101e8d
An0n3m0us <an0n3m0us@hotmail.com>
2021-03-24 20:42:27 +0000

    Modify getKAID function to use localStorage to query KAID

--- src/util/data-util.ts
+++ src/util/data-util.ts
@@ -1,10 +1,15 @@
 function getKAID (): string {
-	// window["./javascript/page-template-package/infra-entry.js"] = {"kaid":"kaid_757721856896775939251306","miniProfilerRequestId":null}
-	if (window.hasOwnProperty("./javascript/page-template-package/infra-entry.js")) {
-		// tslint:disable-next-line:no-any
-		return (window as any)["./javascript/page-template-package/infra-entry.js"].kaid as string;
+	// Get KAID from session
+	const kaids = localStorage["ka:4:mastery_accelerant_prompt"].match(/kaid_\d+/g);
+	const dates = localStorage["ka:4:mastery_accelerant_prompt"].match(/:\d{13}\}/g);
+
+	const combined = [];
+	for (let i = 0; i < kaids.length; i += 1) {
+			combined.push(dates[i] + kaids[i]);
 	}
-	throw new Error("Can't find KAID. Infra Entry not loaded.");
+	combined.sort();
+
+	return combined[combined.length-1].match(/kaid_\d+/g)[0];
 }
 
 export { getKAID };
