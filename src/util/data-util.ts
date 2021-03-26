38ee831e16652ac5e545c96b63cb6fff881738a6
An0n3m0us <an0n3m0us@hotmail.com>
2021-03-25 22:28:55 -0400

    Modify getKAID function to use localStorage to query KAID (#221)
    * Modify getKAID function to use localStorage to query KAID
    
    * Remove blank lines
    
    * Add comment blocks for information

--- src/util/data-util.ts
+++ src/util/data-util.ts
@@ -1,10 +1,19 @@
 function getKAID (): string {
-	// window["./javascript/page-template-package/infra-entry.js"] = {"kaid":"kaid_757721856896775939251306","miniProfilerRequestId":null}
-	if (window.hasOwnProperty("./javascript/page-template-package/infra-entry.js")) {
-		// tslint:disable-next-line:no-any
-		return (window as any)["./javascript/page-template-package/infra-entry.js"].kaid as string;
+	// Get KAID of current session by checking latest entry in localStorage
+
+	// Get all KAID session entries + epoch date
+	const kaids = localStorage["ka:4:mastery_accelerant_prompt"].match(/kaid_\d+/g);
+	const dates = localStorage["ka:4:mastery_accelerant_prompt"].match(/:\d{13}\}/g);
+	const combined = [];
+	// Push into array with sortable format (by date)
+	// Format: $Date$Kaid
+	for (let i = 0; i < kaids.length; i += 1) {
+			combined.push(dates[i] + kaids[i]);
 	}
-	throw new Error("Can't find KAID. Infra Entry not loaded.");
+	combined.sort();
+
+	// Return latest KAID
+	return combined[combined.length-1].match(/kaid_\d+/g)[0];
 }
 
 export { getKAID };
