442037499c73f2e20ac9bb295f9b8ca3c8f936c4
Luke Krikorian <lukekrikorian@gmail.com>
2021-04-21 03:15:20 -0400

    Fix #230: getKAID broken when local storage is cleared

--- src/util/data-util.ts
+++ src/util/data-util.ts
@@ -1,19 +1,9 @@
 function getKAID (): string {
-	// Get KAID of current session by checking latest entry in localStorage
-
-	// Get all KAID session entries + epoch date
-	const kaids = localStorage["ka:4:mastery_accelerant_prompt"].match(/kaid_\d+/g);
-	const dates = localStorage["ka:4:mastery_accelerant_prompt"].match(/:\d{13}\}/g);
-	const combined = [];
-	// Push into array with sortable format (by date)
-	// Format: $Date$Kaid
-	for (let i = 0; i < kaids.length; i += 1) {
-			combined.push(dates[i] + kaids[i]);
+	for (const key in window.__APOLLO_CLIENT__.cache.data.data) {
+		if (key.startsWith("User:kaid_")) {
+			return key.substring("User:".length);
+		}
 	}
-	combined.sort();
-
-	// Return latest KAID
-	return combined[combined.length-1].match(/kaid_\d+/g)[0];
 }
 
 export { getKAID };
