a7f16392e9c503bb522b1a03ab7c28455f9a54ac
MatthiasSaihttam <MatthiasSaihttam1@gmail.com>
2021-04-23 00:02:12 -0400

    Fix editor buttons
    Also removes CSS to make sort buttons blue since KA added that

--- src/buttons.ts
+++ src/buttons.ts
@@ -5,71 +5,76 @@ import { getKAID } from "./util/data-util";
 
 //Replace KA's vote button with one that updates after you vote and allows undoing votes
 function replaceVoteButton (buttons: HTMLDivElement, program: Program): void {
-	const wrap = buttons.querySelector(".voting-wrap .discussion-meta-controls span");
+	buttons.id = "buttons-wrap";
+	querySelectorPromise("#buttons-wrap .discussion-meta-controls > button:first-child").then(wrap => {
 
-	//TODO: Handle non-English
-	if (!(wrap instanceof HTMLElement) || !wrap.innerText.includes("Vote")) {
-		console.log("Voting failed to load.", buttons, wrap, wrap && wrap.firstChild);
-		return;
-	}
+		//TODO: Handle non-English
+		if (!(wrap instanceof HTMLElement) || !wrap.innerText.includes("Vote")) {
+			console.log("Voting failed to load.", buttons, wrap, wrap && wrap.firstChild);
+			return;
+		}
 
-	const VOTE_URL = "/api/internal/discussions/voteentity";
+		const VOTE_URL = "/api/internal/discussions/voteentity";
 
-	let voted = wrap.innerText.includes("Voted Up");
+		let voted = wrap.innerText.includes("Voted Up");
 
-	const orgVotes = program.sumVotesIncremented - (voted ? 1 : 0);
+		const orgVotes = program.sumVotesIncremented - (voted ? 1 : 0);
 
-	const newWrap = document.createElement("span");
-	const voteButton = document.createElement("a");
-	const voteText = document.createElement("span");
-	voteButton.setAttribute("role", "button");
-	voteButton.classList.add("kae-program-button");
-	voteButton.appendChild(voteText);
-	newWrap.appendChild(voteButton);
+		const newWrap = document.createElement("span");
+		const voteButton = document.createElement("button");
+		const voteText = document.createElement("span");
+		voteButton.setAttribute("role", "button");
+		voteButton.classList.add("kae-program-button");
+		voteButton.appendChild(voteText);
+		newWrap.appendChild(voteButton);
 
-	function updateVoteDisplay () {
-		voteText.innerText = (voted ? "Voted Up!" : "Vote Up") + " • " + (orgVotes + (voted ? 1 : 0));
-	}
-	updateVoteDisplay();
-
-	newWrap.addEventListener("click", function () {
-		voted = !voted;
+		function updateVoteDisplay () {
+			voteText.innerText = (voted ? "Voted Up!" : "Vote Up") + " • " + (orgVotes + (voted ? 1 : 0));
+		}
 		updateVoteDisplay();
 
-		fetch(`${VOTE_URL}?entity_key=${program.key}&vote_type=${voted ? 1 : 0}`, {
-			method: "POST",
-			headers: { "X-KA-FKey": getCSRF() },
-			credentials: "same-origin"
-		}).then((response: Response): void => {
-			//If there's an error, undo the vote
-			if (response.status !== 204) {
-				response.json().then((res: { error?: string }): void => {
-					if (res.error) {
-						alert("Failed with error:\n\n" + res.error);
-					}
-				}).catch(() => {
-					alert(`Voting failed with status ${response.status}`);
-				});
-				voted = !voted;
-				updateVoteDisplay();
-			}
-		}).catch(console.error);
-	});
+		newWrap.addEventListener("click", function () {
+			voted = !voted;
+			updateVoteDisplay();
 
-	if (wrap.parentNode) {
-		wrap.parentNode.insertBefore(newWrap, wrap);
-		wrap.parentNode.removeChild(wrap);
-	}
+			fetch(`${VOTE_URL}?entity_key=${program.key}&vote_type=${voted ? 1 : 0}`, {
+				method: "POST",
+				headers: { "X-KA-FKey": getCSRF() },
+				credentials: "same-origin"
+			}).then((response: Response): void => {
+				//If there's an error, undo the vote
+				if (response.status !== 200) {
+					response.json().then((res: { error?: string }): void => {
+						if (res.error) {
+							alert("Failed with error:\n\n" + res.error);
+						}
+					}).catch(() => {
+						alert(`Voting failed with status ${response.status}`);
+					});
+					voted = !voted;
+					updateVoteDisplay();
+				}
+			}).catch(console.error);
+		});
+
+		if (wrap.parentNode) {
+			wrap.parentNode.insertBefore(newWrap, wrap);
+			wrap.parentNode.removeChild(wrap);
+		}
+	});
 }
 
 //Add a "Copy Link" button
 function addLinkButton (buttons: HTMLDivElement, program: Program): void {
-	const copyLinkButton: HTMLAnchorElement = document.createElement("a");
+	const copyLinkButton: HTMLButtonElement = document.createElement("button");
 	copyLinkButton.id = "kae-link-button";
 
+	const copyLinkText:HTMLSpanElement = document.createElement("span");
+	copyLinkText.appendChild(document.createTextNode("Copy Link"));
 	copyLinkButton.setAttribute("role", "button");
-	copyLinkButton.innerHTML = "<span>Copy Link</span>";
+	copyLinkButton.appendChild(copyLinkText);
 	copyLinkButton.classList.add("kae-program-button");
+	copyLinkButton.style.marginLeft = "12px";
 	copyLinkButton.addEventListener("click", function () {
 		if (window.navigator.hasOwnProperty("clipboard")) {
 			window.navigator.clipboard.writeText(`https://khanacademy.org/cs/i/${program.id}`).catch((err: Error) => {
@@ -125,14 +130,15 @@ function findOtherButtons (buttons: HTMLDivElement): void {
 function loadButtonMods (program: Program): void {
 	const kaid:string = getKAID();
 
-	querySelectorPromise(".voting-wrap")
-		.then(votingWrap => votingWrap.parentNode)
+	querySelectorPromise("[data-user-kaid]")
+		.then(userLink => userLink as HTMLAnchorElement)
+		.then(userLink => userLink.parentNode!.parentNode!.firstElementChild)
 		.then(buttons => buttons as HTMLDivElement)
 		.then(buttons => {
-			findOtherButtons(buttons);
+			// findOtherButtons(buttons);
 			addLinkButton(buttons, program);
 			replaceVoteButton(buttons, program);
-			addProgramFlags(buttons, program);
+			// addProgramFlags(buttons, program);
 		});
 
 	querySelectorPromise("#child-account-notice")
