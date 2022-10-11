e7a309872070aa3d7cc2f8299c96985b73cb59c0
vExcess <41875927+vExcess@users.noreply.github.com>
2022-10-11 15:35:01 -0700

    Update new-scratchpad.js

--- src/new-scratchpad.js
+++ src/new-scratchpad.js
@@ -2,7 +2,7 @@
 (_=>{
     let aceEditor;
     
-    let checkIfEditorIsReady = setInterval(function () {
+    let checkIfEditorIsReadyInterval = setInterval(function () {
         aceEditor = document.getElementsByClassName("scratchpad-ace-editor")[0]?.env?.editor;
         
         if (aceEditor !== undefined) {
@@ -15,17 +15,24 @@
             }
             
             
+            // save user code to local storage
             function saveEditorCode () {
-                localStorage.setItem("ka-new-program-" + programType, aceEditor.getValue()); // save user code to localStorage
+                localStorage.setItem("ka-new-program-" + programType, aceEditor.getValue());
             }
-            
             window.addEventListener("beforeunload", saveEditorCode); // save code when the user exits the page
-            
             // save code every minute (in case the browser fails to save code when the page is unloaded, this will ensure that all is not lost)
             setInterval(saveEditorCode, 1000 * 60);
             
             
-            clearInterval(checkIfEditorIsReady); // we can stop checking if the editor is ready
+            // delete the saved code from localStorage when it gets saved to KA
+            document.body.addEventListener("mouseup", function (e) {
+                if (e.toElement.className === "_4w4gmuz") {
+                    localStorage.setItem("ka-new-program-" + programType, "");
+                }
+            });
+            
+            
+            clearInterval(checkIfEditorIsReadyInterval); // we can stop checking if the editor is ready
         }
     }, 100);
 })()
