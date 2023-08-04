3787a11b832c0c4452414e179ee462f027d921a6
Matthias Portzel <MatthiasPortzel@gmail.com>
2023-08-04 14:10:56 -0400

    Safe property access prevents an error for logged-out user

--- src/util/data-util.ts
+++ src/util/data-util.ts
@@ -1,6 +1,6 @@
 // Returns undefined if run before the page has fully loaded
 function getKAID (): string|undefined {
-	for (const key in window.__APOLLO_CLIENT__.cache.data.data) {
+	for (const key in window.__APOLLO_CLIENT__?.cache?.data?.data) {
 		if (key.startsWith("User:kaid_")) {
 			return key.substring("User:".length);
 		}
